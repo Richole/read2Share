9a5918ff30ec08e60fd2775b406a274941937128
Richole <dfyu@xtremeprog.com>
2016-03-25 14:35:57 +0800

    update index

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,6 @@ var request = require('request');
 var config = require('../config.js');
 var pool = require('../models/pool.js');
 var fs = require("fs");
-config.pictureFolderPath = "/home/richole/Pictures/";
 
 exports.showIndex = function (request, response, next) {
   if(request.session.uid) {
